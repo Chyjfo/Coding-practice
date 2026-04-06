1bd5b64de075053b348771925c7589de0dcc8dfa
Chyjfo <khushioraon06@gmail.com>
2026-04-06 11:04:06 +0530

    Implement variable swapping using three variables

---
+++ Swap_3Vriables.java
@@ -0,0 +1,12 @@
+public class Program
+{
+    public static void main(String[] args) {
+    int a=20;
+    int b=30;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    System.out.println("a="+a + "\nb="+b);
+     }
+}
