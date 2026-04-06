ec4c7578ee38783251a25275f3a9a2d85abb1b2e
Chyjfo <khushioraon06@gmail.com>
2026-04-06 10:49:08 +0530

    Add Data_typeSize.c to display memory sizes of types

---
+++ Data_typeSize.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+
+   printf("Required memory size is %d",sizeof(int)); 
+   printf("\nRequired memory size is %d",sizeof(short int));
+   printf("\nRequired memory size is %d",sizeof(long int));
+   printf("\nRequired memory size is %d",sizeof(char));
+   printf("\nRequired memory size is %d",sizeof(float));
+   printf("\nRequired memory size is %d",sizeof(double));
+   printf("\nRequired memory size is %d",sizeof(long double));
+   
+}
